daffe4a9f4c5d1e4abec590c0a4c04bd328ec6ea
Mohaib Rehman <mohibrahman973@gmail.com>
2024-09-29 22:53:34 +0500

    Add the Year in a Js file

--- Script.js
+++ Script.js
@@ -1,13 +1,44 @@
-document.getElementById('calculate').addEventListener('click', function) {
 
-}
-if (dob) {
-    alert('Please Enter a valid date');
+
+var calculateBtn = document.getElementById("calculateBtn");
+calculateBtn.addEventListener("click",()=>{
+  console.log("button");
+
+  var inputDate = new Date(document.getElementById("dataInput").value)
+  var todayDate = new Date();
+  console.log(inputDate)
+  // console.log(todayDate)
+  
+  if(inputDate >  todayDate){
+    alert("Please Select your birth date");
+
     return;
-}
-
-var years = today.getFulYear() - dob.getFulYear();
-var months = today.getmonths() - dob.getmonths();
-var days = today.getdate() - dob.getdate();
-
-if (date)
+  }
+  
+  
+  
+  // Years 
+  var years = todayDate.getFullYear() - inputDate.getFullYear();
+  console.log(years)
+  
+  //Months 
+//   var months = todayDate.getMonth() - inputDate.getMonth();
+//   console.log("months",months)
+  
+  // Days
+//   var days = todayDate.getDate() - inputDate.getDate();
+//   console.log(days)
+  
+  
+//   if(days < 0 ){
+//     months--;
+//     days = days + new Date(todayDate.getFullYear(), todayDate.getMonth()).getDate();
+//   }
+//   if (months < 0) {  
+//     years--;  
+//     months += 12;  
+//   }  
+//     document.getElementById("years").innerText = years;
+//     document.getElementById("months").innerText = months;
+//     document.getElementById("days").innerText = days;
+// })
